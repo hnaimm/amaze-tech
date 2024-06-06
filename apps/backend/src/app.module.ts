de7f55dac34172015e4f4aef0ad7217be88919ce
Haifa <hnaimm0@gmail.com>
2024-06-06 15:13:44 +0300

    FEAT (api): tags module & CRUD apis

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, TagsModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
