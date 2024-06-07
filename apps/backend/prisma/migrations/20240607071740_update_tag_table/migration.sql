98c4f3b95e61d4447ef5a4807fa22f7b525446d3
Haifa <hnaimm0@gmail.com>
2024-06-07 10:19:10 +0300

    FEAT (database): update tag table + migration

---
+++ apps/backend/prisma/migrations/20240607071740_update_tag_table/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - The primary key for the `tag` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `createdAt` on the `tag` table. All the data in the column will be lost.
+  - You are about to drop the column `id` on the `tag` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `tag` table. All the data in the column will be lost.
+  - Made the column `name` on table `tag` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `tag` DROP PRIMARY KEY,
+    DROP COLUMN `createdAt`,
+    DROP COLUMN `id`,
+    DROP COLUMN `updatedAt`,
+    MODIFY `name` VARCHAR(255) NOT NULL,
+    ADD PRIMARY KEY (`name`);
