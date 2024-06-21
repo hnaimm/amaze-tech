a362b45516f8fba73548d79a8871712bd6cacd18
Haifa <hnaimm0@gmail.com>
2024-06-21 11:25:33 +0300

    FEAT (api): remove update tag and get tag by id endpoints

--- apps/backend/src/tags/tags.service.ts
+++ apps/backend/src/tags/tags.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTagDto } from './dto/create-tag.dto';
-import { UpdateTagDto } from './dto/update-tag.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -15,19 +14,6 @@ export class TagsService {
 
   async findAll() {
     return this.prisma.prismaClient.tag.findMany();
-  }
-
-  async findOne(id: string) {
-    return this.prisma.prismaClient.tag.findUnique({
-      where: { id: parseInt(id) },
-    });
-  }
-
-  async update(id: string, updateTagDto: UpdateTagDto) {
-    return this.prisma.prismaClient.tag.update({
-      where: { id: parseInt(id) },
-      data: updateTagDto,
-    });
   }
 
   async remove(id: string) {
